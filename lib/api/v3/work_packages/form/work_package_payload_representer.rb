de83a44f007323f650197000c93df6046aa9a722
Jan Sandbrink <j.sandbrink@finn.de>
2015-02-18 16:43:43 +0100

    use factory method for _links representer
    this will never need to be instantiated by ROAR

--- lib/api/v3/work_packages/form/work_package_payload_representer.rb
+++ lib/api/v3/work_packages/form/work_package_payload_representer.rb
@@ -145,7 +145,7 @@ module API
           end
 
           def work_package_attribute_links_representer(represented)
-            ::API::V3::WorkPackages::Form::WorkPackageAttributeLinksRepresenter.new represented
+            ::API::V3::WorkPackages::Form::WorkPackageAttributeLinksRepresenter.create represented
           end
 
           def description_renderer
